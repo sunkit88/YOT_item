e3046e1f6cfdbaa2a7ad858f1349515773d19032
sunkit88 <31676486+sunkit88@users.noreply.github.com>
2019-12-04 22:19:36 +0800

    Initial commit

---
+++ YOT_item.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+item_raw = pd.read_csv(".\item.csv")
+item_cat_raw = pd.read_csv(".\item_cat.csv")
+item_full_raw = pd.read_csv(".\item_full.csv")
+
+item = item_raw[(item_raw["pcat"] == "PUR")]
+item = item[["desc", "unit", "Item Category Code", "pcat"]]
+
+item_cat = item_cat_raw[(item_cat_raw["pcat"] == "PUR")]
+item_cat = item_cat[["desc", "pcat", "item_cat"]]
+
+item_full = item_full_raw
+
+item_full_join = pd.merge(item_full, item_cat, on='desc', how='left')
+item_full_join = pd.merge(item_full_join, item, on='desc', how='left')
+item_full_join.to_csv("item_full_join.csv", index=0, encoding="utf_8_sig")
