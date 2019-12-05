d51d02216ae7a784d362490200e928a74db00757
sunkit@gmail.com <Winsy767>
2019-12-05 17:42:03 +0800

    finish

--- YOT_item.py
+++ YOT_item.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 item_raw = pd.read_csv(".\item.csv")
 item_cat_raw = pd.read_csv(".\item_cat.csv")
-item_full_raw = pd.read_csv(".\item_full.csv")
+item_full_raw = pd.read_csv(".\item_full.csv", dtype={'scode':str})
 
 item = item_raw[(item_raw["pcat"] == "PUR")]
 item = item[["desc", "unit", "Item Category Code", "pcat"]]
@@ -14,4 +14,8 @@ item_full = item_full_raw
 
 item_full_join = pd.merge(item_full, item_cat, on='desc', how='left')
 item_full_join = pd.merge(item_full_join, item, on='desc', how='left')
-item_full_join.to_csv("item_full_join.csv", index=0, encoding="utf_8_sig")
+
+item_full_melt = pd.melt(item_full_join, id_vars =['desc'], value_vars =['scode', 'bcode', 'bcode2'])
+item_full_join.to_csv(".\item_full_join.csv", index=0, encoding="utf_8_sig")
+item_full_melt.to_csv(".\item_full_melt.csv", index=0, encoding="utf_8_sig")
+
